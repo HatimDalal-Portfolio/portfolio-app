eef359ff1d9b637e9116e81e4ab27477aa7757e7
HatimDalal <dalalhatim16@gmail.com>
2025-01-28 13:07:12 +0530

    NavBar changes

--- src/compnents/Navbar.jsx
+++ src/compnents/Navbar.jsx
@@ -1,21 +1,39 @@
 import logo from '../assets/new.png';
-import {FaLinkedinIn} from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
-
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between py-1">
-        <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-16" src={logo} alt="logo" />
-        </div>
+      {/* Logo Section */}
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-16" src={logo} alt="logo" />
+      </div>
 
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      {/* Social Icons Section */}
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/hatimdalalin"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-blue-600"
+        >
           <FaLinkedinIn />
-          <FaGithub/>
-        </div>
+        </a>
+
+        {/* GitHub */}
+        <a
+          href="https://github.com/HatimDalal"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-600"
+        >
+          <FaGithub />
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
